1e38e195798014c36d30fae36ab3d797b87154a8
judah macbook air <cooperjudah@gmail.com>
2024-03-26 16:06:18 -0400

    Bump version: 0.11.0 → 0.12.0

--- hdata/__init__.py
+++ hdata/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Judah Cooper"""
 __email__ = 'judah@hyperdata.network'
-__version__ = '0.11.0'
+__version__ = '0.12.0'
 
 from .models import Source
 from .functions import transform
